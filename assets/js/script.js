265b2a33288b74f814a80ce2f958168c98530fd0
Gabriel Neves <110125888+gabrielneves17@users.noreply.github.com>
2022-10-20 21:11:59 -0300

    Criada toda a funcionalidade do relógio.

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let agora = new Date();
+    let hr = agora.getHours();
+    let min = agora.getMinutes();
+    let seg = agora.getSeconds();
+
+    if(hr < 10) {
+        hr = '0' + hr;
+    }
+    if(min < 10) {
+        min = '0' + min;
+    }
+    if(seg < 10) {
+        seg = '0' + seg;
+    }
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+})
